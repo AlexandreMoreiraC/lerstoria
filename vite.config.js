2f63132725f26585ed78778437c0ff54579cbbb8
Alexandre Moreira <alexandremoreira_.m@hotmail.com>
2026-04-04 20:45:17 +0100

    Fix: configurando páginas múltiplas no Vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        signup: resolve(__dirname, 'signup.html'),
         library: resolve(__dirname, 'library.html'),
         book: resolve(__dirname, 'book.html'),
+        quiz: resolve(__dirname, 'quiz.html'),
       },
     },
   },
-})
+});
